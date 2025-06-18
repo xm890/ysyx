2c7e2ee7abf0c3acf7a999a352d485b73a5b9f89
Liu Hanwen <lhwen1998@std.uestc.edu.cn>
2025-06-18 20:23:06 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard fd19d9b07af12e48d442f6057f65a75114cd9755 Merge pull request #46 from Wenz-jam/master

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 int main() {
-  printf("Hello, ysyx!\n");
+	int a = rand()&1;
+	int b = rand()&1;
+	top->a = a;
+	top->b = b;
+	top->eval();	
+  printf("a=%d,b=%d,f=%d\n",a,b,top->f);
+	assert(top->f==(a^b));
   return 0;
 }
