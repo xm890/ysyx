5b0e1596d92fe121ecfe0254acd52e6f633f3a17
Liu Hanwen <lhwen1998@std.uestc.edu.cn>
2025-06-18 21:55:29 +0800

    lhw

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vtop.h"
+int main(int argc,char ** argv, char ** env) {
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc,argv);
+	Vtop *top=new Vtop;
+	int i=10;
 
-int main() {
-	int a = rand()&1;
-	int b = rand()&1;
-	top->a = a;
-	top->b = b;
-	top->eval();	
-  printf("a=%d,b=%d,f=%d\n",a,b,top->f);
-	assert(top->f==(a^b));
+	VerilatedVcdC *tfp=new VerilatedVcdC;
+	contextp->traceEverOn(true);
+	top->trace(tfp,0);
+	tfp->open("wave.vcd");
+	while(i--){
+		int a = rand()&1;
+		int b = rand()&1;
+		top->a = a;
+		top->b = b;
+		top->eval();	
+  	printf("a=%d,b=%d,f=%d\n",a,b,top->f);
+		tfp->dump(contextp->time());
+		contextp->timeInc(1);
+		assert(top->f==(a^b));
+	}
+	delete top;
+	tfp->close();
+	delete contextp;
   return 0;
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,7 @@
 module top(
-	input a;
-	input b;
-	output f;
+	input a,
+	input b,
+	output f
 );
 	assign f = a^b;
 endmodule
