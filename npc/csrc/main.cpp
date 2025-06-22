be83172bace14f947299949cac1a3da2b372c3b0
Liu Hanwen <lhwen1998@std.uestc.edu.cn>
2025-06-22 20:09:45 +0800

    running led

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,32 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <verilated.h>
-#include <verilated_vcd_c.h>
-#include "Vtop.h"
-int main(int argc,char ** argv, char ** env) {
-	VerilatedContext* contextp = new VerilatedContext;
-	contextp->commandArgs(argc,argv);
-	Vtop *top=new Vtop;
-	int i=10;
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<nvboard.h>
 
-	VerilatedVcdC *tfp=new VerilatedVcdC;
-	contextp->traceEverOn(true);
-	top->trace(tfp,0);
-	tfp->open("wave.vcd");
-	while(i--){
-		int a = rand()&1;
-		int b = rand()&1;
-		top->a = a;
-		top->b = b;
-		top->eval();	
-  	printf("a=%d,b=%d,f=%d\n",a,b,top->f);
-		tfp->dump(contextp->time());
-		contextp->timeInc(1);
-		assert(top->f==(a^b));
-	}
-	delete top;
-	tfp->close();
-	delete contextp;
-  return 0;
+#include"Vtop.h"  
+#include"verilated.h"
+#include"verilated_vcd_c.h"
+
+void nvboard_bind_all_pins(Vtop* top);
+void single_cycle(Vtop* top){
+	top->clk=0;top->eval();
+	top->clk=1;top->eval();
 }
+void reset(Vtop* top,int n){
+	top->rst = 1;
+	while(n-- > 0) 
+		single_cycle(top);
+	top->rst = 0;
+}
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->traceEverOn(true);
+    contextp->commandArgs(argc, argv);
+
+    Vtop* top = new Vtop{contextp};
+
+    nvboard_bind_all_pins(top);  
+    nvboard_init();
+
+    VerilatedVcdC* m_trace = new VerilatedVcdC; 
+    top->trace(m_trace, 99);  
+    m_trace->open("wave.vcd");  
+    
+	reset(top,10);
+    while (!contextp->gotFinish()) {
+		nvboard_update();
+        contextp->timeInc(1);
+		single_cycle(top);
+        m_trace->dump(contextp->time()); 
+    }
+    m_trace->close();
+    delete top;
+    delete contextp;
+    return 0;
+}
+
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,15 @@
-module top(
-	input a,
-	input b,
-	output f
+module top(  // 这里名字要与Makefile中的TOPNAME和cpp文件中的#include "Vtop.h"相同
+  input clk,
+  input rst,
+  output reg [15:0] led
 );
-	assign f = a^b;
+  reg [31:0] count;
+  always @(posedge clk) begin
+    if (rst) begin led <= 1; count <= 0; end
+    else begin
+      if (count == 0) led <= {led[14:0], led[15]}; // 后四位：0001->0010->0100->1000 为1的位亮灯
+      count <= (count >= 5000000 ? 32'b0 : count + 1); // 控制亮灯间隔
+    end
+  end
 endmodule
+
